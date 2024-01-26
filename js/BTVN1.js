4fa3480d175fd75111dedb66a257eb0a97cc928b
LaamTrg <lamcupeo@gmail.com>
2024-01-26 21:28:25 +0700

    BTVN1 Commit

--- js/BTVN1.js
+++ js/BTVN1.js
@@ -1,19 +1,19 @@
-function muaVe() {
+function muaVe(){
     var ve = [];
-    while (ve.length < 6) {
+    while (ve.length < 6){
         var soNgauNhien = Math.floor(Math.random() * 46);
-        if (ve.indexOf(soNgauNhien) === -1) {
+        if (ve.indexOf(soNgauNhien) === -1){
             ve.push(soNgauNhien);
         }
     }
     return ve;
 }
 
-function nhapVe() {
+function nhapVe(){
     var ve = [];
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 6; i++){
         var so = parseInt(prompt("Nhập số thứ " + (i + 1) + " (từ 0 đến 45):"));
-        while (isNaN(so) || so < 0 || so > 45 || ve.includes(so)) {
+        while (isNaN(so) || so < 0 || so > 45 || ve.includes(so)){
             alert("Vui lòng nhập một số hợp lệ và chưa được chọn trước đó.");
             so = parseInt(prompt("Nhập số thứ " + (i + 1) + " (từ 0 đến 45):"));
         }
@@ -22,18 +22,18 @@ function nhapVe() {
     return ve;
 }
 
-function kiemTraTrungGiai(veNguoiChoi, veMay) {
+function kiemTraTrungGiai(veNguoiChoi, veMay){
     var soTrungGiai = 0;
     for (var i = 0; i < veNguoiChoi.length; i++) {
         var so = veNguoiChoi[i];
-        if (veMay.indexOf(so) !== -1) {
+        if (veMay.indexOf(so) !== -1){
             soTrungGiai++;
         }
     }
     return soTrungGiai;
 }
 
-function choiVietlot() {
+function choiVietlot(){
     var veNguoiChoi = nhapVe();
     alert("Vé của bạn: " + veNguoiChoi.join(", "));
 
@@ -43,15 +43,15 @@ function choiVietlot() {
     var soTrungGiai = kiemTraTrungGiai(veNguoiChoi, veMay);
     alert("Số vé bạn trúng: " + soTrungGiai);
 
-    if (soTrungGiai == 0 || soTrungGiai == 1 || soTrungGiai == 2) {
+    if(soTrungGiai == 0 || soTrungGiai == 1 || soTrungGiai == 2){
         alert("Chúc may mắn lần sau.");
-    } else if (soTrungGiai == 3) {
+    }else if (soTrungGiai == 3){
         alert("Chúc mừng bạn trúng 100.000 VND");
-    } else if (soTrungGiai == 4) {
+    }else if (soTrungGiai == 4){
         alert("Chúc mừng bạn trúng 10.000.000 VND");
-    } else if (soTrungGiai == 5) {
+    }else if (soTrungGiai == 5){
         alert("Chúc mừng bạn trúng 100 tỷ VND");
-    } else if (soTrungGiai == 6) {
+    }else if (soTrungGiai == 6){
         alert("Chúc mừng bạn trúng 1.000 tỷ VND");
     }
 }
